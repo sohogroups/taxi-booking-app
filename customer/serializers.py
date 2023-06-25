8438f50c11c05d2dd275accdfe9a2baf9ba4490d
sohogroups <136905964+sohogroups@users.noreply.github.com>
2023-06-25 19:27:46 +0530

    Update serializers.py

--- customer/serializers.py
+++ customer/serializers.py
@@ -5,3 +5,8 @@ class CustomerSerializers(serializers.ModelSerializer):
 	class Meta:
 		model = Customers
 		fields = ['name','email','phone_number','gender']
+		
+class CustomerProfileSerializers(serializers.ModelSerializer):
+	class Meta:
+		model = Customers
+		fields = ['name','email','phone_number','gender','street','city','district']
